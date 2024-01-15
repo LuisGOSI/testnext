c9d98fefb7ed6f49d207edca29d9939ee63e340f
Luis Ignacio Gonzalez Silva <luis_gosi@outlook.com>
2024-01-15 12:47:25 -0600

    mejoras de interfaz

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,41 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
-        <li>
-          <Link href="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/pages/test">
-            Test
-          </Link>
-        </li>
-      </ul>
-    </main>
-  )
+    <div className="container mx-auto">
+      {/* Encabezado */}
+      <header className="my-8">
+        <h1 className="text-4xl font-bold">Pagina hecha con Next</h1>
+      </header>
+
+      {/* Barra de navegación */}
+      <nav className="my-4">
+        <ul className="flex">
+          <li className="mr-4">
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/pages/test">
+              Test
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <main className="my-8">
+        <section>
+          <h2 className="text-2xl font-bold mb-4">Publicaciones Recientes</h2>
+          Pagina pendiente de terminar
+        </section>
+
+      </main>
+
+      {/* Pie de página */}
+      <footer className="py-4 border-t">
+        <p>&copy; 2024 Luis Ignacio Gonzalez Silva</p>
+      </footer>
+    </div>
+  );
 }
--- app/pages/test/page.tsx
+++ app/pages/test/page.tsx
@@ -1,5 +1,27 @@
+import Link from 'next/link'
+
 export default function Test() {
-    return <div>
-        Pagina de prueba
-    </div>
+    return (
+        <main>
+            <h1 className="text-4xl font-bold">
+                Pagina de prueba
+            </h1>
+            <nav className="my-4">
+                <ul className="flex">
+                    <li className="mr-4">
+                        <Link href="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/pages/test">
+                            Test
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </main>
+    )
+
 }
+
