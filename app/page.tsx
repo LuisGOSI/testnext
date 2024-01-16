d2f3b47c29cb21557b29f6cda36b6bb09e6eb8b8
Luis Ignacio Gonzalez Silva <luis_gosi@outlook.com>
2024-01-16 12:27:59 -0600

    Version de la clase

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,77 @@
-import Link from 'next/link';
+// import Link from 'next/link';
+// import Head from 'next/head';
+import styles from '@/styles/styles.module.css';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      {/* Encabezado */}
-      <header className="my-8">
-        <h1 className="text-4xl font-bold">Pagina hecha con Next</h1>
-      </header>
+    <div className={styles.container}>
+      {/* <Head>
+        <title>Mi página Web</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=devide-width, initial-scale=1, shrink-to-fit=no" />
+      </Head> */}
 
-      {/* Barra de navegación */}
-      <nav className="my-4">
-        <ul className="flex">
-          <li className="mr-4">
-            <Link href="/">
-              Home
-            </Link>
+      {/* Navbar */}  
+      <nav className={styles.navbar}>
+        <a href="#" className={styles.navbarBrand}>Home</a>
+        <ul className={styles.navbarNav}>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Ayuda</a>
           </li>
-          <li>
-            <Link href="/pages/test">
-              Test
-            </Link>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Contacto</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Sesion</a>
           </li>
         </ul>
       </nav>
 
-      <main className="my-8">
-        <section>
-          <h2 className="text-2xl font-bold mb-4">Publicaciones Recientes</h2>
-          Pagina pendiente de terminar
-        </section>
-
-      </main>
-
-      {/* Pie de página */}
-      <footer className="py-4 border-t">
-        <p>&copy; 2024 Luis Ignacio Gonzalez Silva</p>
-      </footer>
+      {/* Contenido principal */}
+      <div className={styles.content}>
+        <h1>Development with Next</h1>
+      </div>
+        
+        {/* Footer */}
+        <Footer/>
     </div>
-  );
+  )
 }
+
+
+// <div className="container mx-auto">
+//   {/* Encabezado */}
+//   <header className="my-8">
+//     <h1 className="text-4xl font-bold">Pagina hecha con Next</h1>
+//   </header>
+
+//   {/* Barra de navegación */}
+//   <nav className="my-4">
+//     <ul className="flex">
+//       <li className="mr-4">
+//         <Link href="/">
+//           Home
+//         </Link>
+//       </li>
+//       <li>
+//         <Link href="/pages/test">
+//           Test
+//         </Link>
+//       </li>
+//     </ul>
+//   </nav>
+
+//   <main className="my-8">
+//     <section>
+//       <h2 className="text-2xl font-bold mb-4">Publicaciones Recientes</h2>
+//       Pagina pendiente de terminar
+//     </section>
+
+//   </main>
+
+//   {/* Pie de página */}
+//   <footer className="py-4 border-t">
+//     <p>&copy; 2024 Luis Ignacio Gonzalez Silva</p>
+//   </footer>
+// </div>
---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import styles from '@/styles/styles.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <div>
+                <p>
+                    Derechos reservados | &copy; 2024 Luis Ignacio Gonzalez Silva
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+
+export default Footer;
